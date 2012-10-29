885839eef431696812e4686031c9af59c60cdd39
TuKod <lan@lan.(none)>
2012-10-29 12:35:08 +0800

    Signed-off-by: TuKod

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Allow YOURLS with CMS
+Plugin URI: http://tukod.com/tukod/allow-yourls-with-cms/
+Description: Allows YOURLS to work on the same domain name with a CMS like WordPress, Mambo, or Joomla.  See the <a href="http://github.com/ggwarpig/Yourls-Swap-Short-Url/blob/master/README.md">instructions</a>
+Version: 1.0
+Author: TuKod
+Author URI: http://alantait.net/
+*/
+
+// List of plugin hooks http://yourls.org/hooklist.php
+
+// Modifies the custom keyword to add a CMS safety character.
+// functions-html.php 196
+yourls_add_filter( 'custom_keyword', 'tukod_add_cms_safe_char' );
+
+// Modifies the random keyword to add a CMS safety character.
+// functions-html.php 223
+yourls_add_filter( 'random_keyword', 'tukod_add_cms_safe_char' );
+
+function tukod_add_cms_safe_char( $args ) {
+	return TUKOD_YOURLS_CMS_PREFIX . $args['keyword'];
+}
